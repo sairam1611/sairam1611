1b999317a574d492d954e6cd40f4a5067e74f4ab
sairam1611 <sairamch518@gmail.com>
2024-07-19 07:32:28 +0530

    Add files via upload

---
+++ firstprogram.java
@@ -0,0 +1,19 @@
+package firstproject;
+
+public class firstprogram
+{
+	public static void main ( String[] args)
+	{
+		System.out.println("my name is sairam");
+		System.out.println("i am studying mca");
+		System.out.println("i am from nalgonda");
+		System.out.println("i am studying in avanthi pg college");
+		System.out.println("it is located in hyderabad");
+		System.out.println("i have completed my degree in nalgonda");
+		System.out.println("i am studied in sri raghavendra degree college");
+		System.out.println("i have completed my degree in B.COM(computer applications)");
+		System.out.println("i am living in hyderabad");
+		System.out.println("my native place is munugode");
+	}
+
+}
